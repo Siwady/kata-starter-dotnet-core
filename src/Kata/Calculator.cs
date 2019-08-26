512c1469acdbde872857531741e2d1a89663071a
csolorzanof <illgamer@hotmail.com>
2019-08-26 14:39:54 -0600

    unknown number of numbers passing, new line delimter failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -8,12 +8,11 @@ namespace Kata
         {
             if (string.IsNullOrEmpty(s))
                 return 0;
-            if (s.Contains(","))
-            {
-                var numbers = s.Split(",").Select(int.Parse);
-                return numbers.First() + numbers.Last();
-            }
-            return 1;
+            
+            
+            var numbers = s.Split(",").Select(int.Parse);
+            return numbers.Sum();
+            
         }
     }
 }
