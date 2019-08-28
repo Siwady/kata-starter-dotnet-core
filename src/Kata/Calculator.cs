c2f540f28bc7662bc962287f44d54ab9614603c6
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-08-28 14:36:51 -0600

    one negative passing, multiple negatives failling

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -7,18 +7,25 @@ namespace Kata
     {
         public int Add(string s="")
         {
-            var delimitors = new[] {",", "\n"};
+            var delimiters = new[] {",", "\n"};
             if (string.IsNullOrEmpty(s))
                 return 0;
             
             if (s.Contains("//"))
             {
                 var text = s.Split("\n");
-                delimitors = new[] {text[0].Replace("//", "")};
+                delimiters = new[] {text[0].Replace("//", "")};
                 s = text[1];
             }
 
-            var numbers = s.Split(delimitors, StringSplitOptions.None).Select(int.Parse);
+            var numbers = s.Split(delimiters, StringSplitOptions.None).Select(int.Parse);
+
+            var negatives = numbers.Where(x => x < 0);
+
+            if (negatives.Any())
+            {
+                throw new Exception($"negatives not allowed: {negatives.First()}");
+            }
 
             return numbers.Sum();
         }
