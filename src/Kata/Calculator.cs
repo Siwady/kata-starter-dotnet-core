491a18265e5ea08b5b487eadd8a7d705f9b5b918
ESiwady <mac@MacBook-Pro-de-mac.local>
2019-08-28 14:31:56 -0600

    custom delimeter passing, negative number failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -7,10 +7,18 @@ namespace Kata
     {
         public int Add(string s="")
         {
+            var delimitors = new[] {",", "\n"};
             if (string.IsNullOrEmpty(s))
                 return 0;
+            
+            if (s.Contains("//"))
+            {
+                var text = s.Split("\n");
+                delimitors = new[] {text[0].Replace("//", "")};
+                s = text[1];
+            }
 
-            var numbers = s.Split(new[]{",","\n"}, StringSplitOptions.None).Select(int.Parse);
+            var numbers = s.Split(delimitors, StringSplitOptions.None).Select(int.Parse);
 
             return numbers.Sum();
         }
