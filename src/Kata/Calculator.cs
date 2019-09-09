7f6073f0313f195bf3b9a782d41d52901f0021fa
ESiwady <mac@MacBook-Pro-de-mac.local>
2019-09-09 14:19:34 -0600

    custom delimiter passing, negative number failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -7,12 +7,19 @@ namespace Kata
     {
         public int Add(string s="")
         {
+            var delimiters = new[] {",", "\n"};
             if (string.IsNullOrEmpty(s))
             {
                 return 0;
             }
 
-            var numbers = s.Split(new []{",","\n"},StringSplitOptions.None).Select(int.Parse);
+            if (s.Contains("//"))
+            {
+                var tmpInput = s.Split("\n");
+                delimiters = new[] {tmpInput[0].Replace("/", "")};
+                s = tmpInput[1];
+            }
+            var numbers = s.Split(delimiters,StringSplitOptions.None).Select(int.Parse);
             
             return numbers.Sum();
         }
